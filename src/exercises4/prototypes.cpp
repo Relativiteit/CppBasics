e01f7fb0529a71e2a5d314c04505637d767edb09
Relativiteit <alejocain@gmail.com>
2023-07-25 15:50:00 +0200

    cpp file cleanup

--- src/exercises4/prototypes.cpp
+++ src/exercises4/prototypes.cpp
@@ -1,23 +1,10 @@
 #include <iostream>
+#include "my_computation.hpp"
 
 using namespace std;
-// prototypes
-int triple_number(int number);
-void print_triple_number(int number);
 
 int main()
 {
-
     print_triple_number(4);
     return 0;
-}
-
-int triple_number(int number)
-{
-    return number * 3;
-}
-
-void print_triple_number(int number)
-{
-    cout << triple_number(number) << endl;
 }
